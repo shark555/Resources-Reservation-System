e2e8862764d416c40814bedb0c6ea362e49d373d
shark555 <shark555@gmail.com>
2010-05-19 13:59:52 +0200

    -Tablica checkbuttony -Metoda dla przycisku Usuń tematy -Usuwanie działa -Wyświetlanie ID tematu na liście -Ostrzeżenie w razie niezaznaczenia tematów -Odświeżanie po usunięciu

--- Code/gl/src/GUI/GUIMain.cs
+++ Code/gl/src/GUI/GUIMain.cs
@@ -17,6 +17,13 @@ public class GUIMain{
 		Label HelloLabel;
 	[Glade.Widget]
 		public static Table TopicTable;
+	[Glade.Widget]
+		Button DeleteTopic;
+	
+	
+	public static CheckButton[] checkbuttony = new CheckButton[100];
+	public static int iletematow = 0;
+	
 
 	public GUIMain(){
 		Glade.XML gxml = new Glade.XML("../../src/GUI/Glade/ekranglowny.glade", "MainWindow", null);
@@ -33,6 +40,7 @@ public class GUIMain{
 		m_quit.ButtonPressEvent += OnPressMenuQuitEvent;
 		m_about.ButtonPressEvent += OnPressMenuAboutEvent;
 		AddTopic.Clicked += OnPressAddTopicEvent;
+		DeleteTopic.Clicked += OnPressDeleteTopicEvent;
 	}
 	
 	//do zrobienia - scroll listy tematów!
@@ -46,17 +54,24 @@ public class GUIMain{
 		Label dataDoLabel = new Label("Data zakończenia");
 		Label kategoriaLabel = new Label("Kategoria");
 		
+
+		
 		//czyszczenie tablicy - zeby nie nadpisywać tego, co było
 		Gtk.Widget[] dzieci = TopicTable.Children;
 		for (int i = 0; i < dzieci.Length; i++)
 			TopicTable.Remove(dzieci[i]);
 		
-		//wypełnianie tabeli nagłówkami
-		TopicTable.Attach(nazwaLabel, 0, 1, 0, 1);
-		TopicTable.Attach(dataOdLabel, 1, 2, 0, 1);
-		TopicTable.Attach(dataDoLabel, 2, 3, 0, 1);
-		TopicTable.Attach(kategoriaLabel, 3, 4, 0, 1);
 		
+		
+	
+		
+		//wypełnianie tabeli nagłówkami
+		TopicTable.Attach(new Label("ID"), 0, 1, 0, 1);
+		TopicTable.Attach(nazwaLabel, 1, 2, 0, 1);
+		TopicTable.Attach(dataOdLabel, 2, 3, 0, 1);
+		TopicTable.Attach(dataDoLabel, 3, 4, 0, 1);
+		TopicTable.Attach(kategoriaLabel, 4, 5, 0, 1);
+
 		//pobranie userid z bazy
 		param += "\'" + UserList.getInstance().current().imie + "\' ";
 		param += "AND Nazwisko=\'" + UserList.getInstance().current().nazwisko + "\';";
@@ -68,21 +83,33 @@ public class GUIMain{
 		
 		DBQuery.CloseReader(reader);
 		
+		
+		
+		
 		//pobranie tematów użytkownika i wpisanie ich do tabeli		
 		param = " * FROM Subjects WHERE UserID=\'" + userID + "\';";
 		reader = DBQuery.createQuery("SELECT", param);
+		int k=0;
 		while (reader.Read()){
 			isEmpty = false;
+			Label idLabelTmp = new Label(reader["id"].ToString());
 			Label nazwaLabelTmp = new Label(reader["Topic"].ToString());
 			Label dataOdLabelTmp = new Label(reader["DateFrom"].ToString());
 			Label dataDoLabelTmp = new Label(reader["DateTo"].ToString());
 			kategoriaLabel = new Label(reader["Cathegory"].ToString());
 		
-			TopicTable.Attach(nazwaLabelTmp, 0, 1, curTop, curBot);
-			TopicTable.Attach(dataOdLabelTmp, 1, 2, curTop, curBot);
-			TopicTable.Attach(dataDoLabelTmp, 2, 3, curTop, curBot);
-			TopicTable.Attach(kategoriaLabel, 3, 4, curTop++, curBot++);
+			TopicTable.Attach(idLabelTmp, 0, 1, curTop, curBot);
+			TopicTable.Attach(nazwaLabelTmp, 1, 2, curTop, curBot);
+			TopicTable.Attach(dataOdLabelTmp, 2, 3, curTop, curBot);
+			TopicTable.Attach(dataDoLabelTmp, 3, 4, curTop, curBot);
+			TopicTable.Attach(kategoriaLabel, 4, 5, curTop, curBot);
+			checkbuttony[k] = new CheckButton();
+			checkbuttony[k].Name = idLabelTmp.Text;
+			TopicTable.Attach((checkbuttony[k]),5,6,curTop++,curBot++);
+			k++;
 		}
+			
+		iletematow = k;
 		
 		DBQuery.CloseReader(reader);
 		
@@ -112,4 +139,40 @@ public class GUIMain{
 	private void OnPressAddTopicEvent(object o, EventArgs e){
 		new GUIAddTopic();
     }
+	
+	private void OnPressDeleteTopicEvent(object o, EventArgs e){
+    
+	int j = 0;	
+	
+	for(int i=0;i<iletematow;i++) {
+
+			
+	if(checkbuttony[i].Active) {
+	Console.WriteLine(checkbuttony[i].Name);
+	String param = " FROM Subjects WHERE ID="+checkbuttony[i].Name;
+	IDataReader reader = DBQuery.createQuery("DELETE", param);
+	DBQuery.CloseReader(reader);	
+	j++;
+	}
+	
+	}
+	
+	if(j==0) { 
+		
+	Gtk.MessageDialog msgDialog = new Gtk.MessageDialog(null, 
+			                                                DialogFlags.DestroyWithParent, 
+                                  							MessageType.Info,
+                                  							ButtonsType.Ok, 
+			                                                "Nie zaznaczono żadnego tematu");
+			
+	int res = msgDialog.Run();
+	msgDialog.Destroy();
+		
+	}	
+	
+	loadTopics();					
+
+	}
+	
+	
 }
